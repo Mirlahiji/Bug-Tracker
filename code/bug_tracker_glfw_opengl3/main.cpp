c4ee8634172012be6234855526b689bd4d9aee6c
Delaram Mirlahiji <87209891+Mirlahiji@users.noreply.github.com>
2023-04-21 22:49:30 +0430

    tabs and clean folders

--- code/bug_tracker_glfw_opengl3/main.cpp
+++ code/bug_tracker_glfw_opengl3/main.cpp
@@ -124,14 +124,32 @@ int main(int, char**)
             ImGui::SetNextWindowSize(ImVec2(500,500));
             if(ImGui::Begin("Hello!",NULL,ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
             {
-                static bool testbool = false;
-                Checkbox("test bool",&testbool);
-
-                if (Button("click me!")) {
-
+                //Make frame of everything round
+                style.FrameRounding = 6.0f;
+                style.GrabRounding = style.FrameRounding;
+                ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None;
+                if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags))
+                {
+                    if (ImGui::BeginTabItem("Overview"))
+                    {
+                        ImGui::SeparatorText("Status");
+                        ImGui::Text("Ready");
+                        ImGui::Text("In progress");
+                        ImGui::Text("Done");
+                        ImGui::SeparatorText("Priority");
+                        ImGui::SeparatorText("People");
+                        ImGui::EndTabItem();
+                    }
+                    
+                    if (ImGui::BeginTabItem("Issues"))
+                    {
+                        ImGui::SeparatorText("People");
+                        ImGui::EndTabItem();
+                    }
                 }
-
-            }ImGui::End();
+                ImGui::EndTabBar();
+            }
+            ImGui::End();
         }
 
         // Rendering
